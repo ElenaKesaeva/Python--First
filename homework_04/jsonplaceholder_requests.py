6942c44f7fe286d3ffe77d7ce11af4bf09ff6b7d
elenakesaeva <1234>
2022-11-07 17:12:14 -0800

    add changes in homework_04

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -2,21 +2,24 @@
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
 import aiohttp
+from aiohttp import ClientSession
 
 USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
-async def fetch_users_data(session: aiohttp.ClientSession):
-    async with session.get(USERS_DATA_URL) as response:
-        users_data = await response.json()
-        return users_data
+async def fetch_users_data():
+    async with ClientSession() as session:
+        async with session.get(USERS_DATA_URL) as response:
+            users_data = await response.json()
+            return users_data
 
 
-async def fetch_posts_data(session: aiohttp.ClientSession):
-    async with session.get(POSTS_DATA_URL) as response:
-        posts_data = await response.json()
-        return posts_data
+async def fetch_posts_data():
+    async with ClientSession() as session:
+        async with session.get(POSTS_DATA_URL) as response:
+            posts_data = await response.json()
+            return posts_data
 
 
 
